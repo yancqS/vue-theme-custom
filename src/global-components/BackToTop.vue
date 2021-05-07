d8c13c5855548372758f651fb2f62336c79a1123
闫崇庆 <23746485@qq.com>
2021-05-07 19:31:03 +0800

    update plugins

--- src/global-components/BackToTop.vue
+++ src/global-components/BackToTop.vue
@@ -3,32 +3,34 @@
 </template>
 
 <script>
-const ua = navigator.userAgent.toLowerCase();
-
-const isChrome = ua.includes("chrome") && ua.includes("safari") && !ua.includes("edge");
-const isFirefox = ua.includes("firefox");
-
 export default {
   data() {
     return {
       top: 0,
+      ua: '',
+      isChrome: '',
+      isFirefox: ''
     };
   },
   mounted() {
+    this.ua = navigator.userAgent.toLowerCase();
+    this.isChrome =
+      this.ua.includes("chrome") && this.ua.includes("safari") && !this.ua.includes("edge");
+    this.isFirefox = this.ua.includes("firefox");
     window.onscroll = (e) => {
       this.top = document.documentElement.scrollTop;
     };
   },
   methods: {
     backtotop() {
-      if (isChrome || isFirefox ) {
+      if (this.isChrome || this.isFirefox) {
         window.scrollTo({
           top: 0,
           behavior: "smooth",
         });
       } else {
         let cb = () => {
-          if(document.documentElement.scrollTop===0) {
+          if (document.documentElement.scrollTop === 0) {
             this.top = 0;
             return;
           } else {
@@ -36,7 +38,7 @@ export default {
             this.top = document.documentElement.scrollTop -= speed;
             requestAnimationFrame(cb);
           }
-        }
+        };
         requestAnimationFrame(cb);
       }
     },
