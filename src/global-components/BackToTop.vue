d36eec862f409b76d36f55ab911f1a35cf3e76d9
闫崇庆 <23746485@qq.com>
2021-05-07 17:47:18 +0800

    ployfill scrollTo Option :bug:

--- src/global-components/BackToTop.vue
+++ src/global-components/BackToTop.vue
@@ -3,34 +3,54 @@
 </template>
 
 <script>
+const ua = navigator.userAgent.toLowerCase();
+
+const isChrome = ua.includes("chrome") && ua.includes("safari") && !ua.includes("edge");
+const isFirefox = ua.includes("firefox");
+
 export default {
   data() {
     return {
-      top: 0
-    }
+      top: 0,
+    };
   },
   mounted() {
     window.onscroll = (e) => {
-      this.top = document.documentElement.scrollTop || document.body.scrollTop;
-    }
+      this.top = document.documentElement.scrollTop;
+    };
   },
   methods: {
     backtotop() {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-      })
-    }
-  }
-}
+      if (isChrome || isFirefox ) {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      } else {
+        let cb = () => {
+          if(document.documentElement.scrollTop===0) {
+            this.top = 0;
+            return;
+          } else {
+            let speed = 50;
+            this.top = document.documentElement.scrollTop -= speed;
+            requestAnimationFrame(cb);
+          }
+        }
+        requestAnimationFrame(cb);
+      }
+    },
+  },
+};
 </script>
 
 <style lang="stylus" scoped>
-.backToTop
-  position fixed
-  bottom 3vh
-  right 2vw
-  transform rotate(-45deg)
-  font-size 2rem
-  cursor pointer
+.backToTop {
+  position: fixed;
+  bottom: 3vh;
+  right: 2vw;
+  transform: rotate(-45deg);
+  font-size: 2rem;
+  cursor: pointer;
+}
 </style>
